c819024afeddf32919c41aefb2b16d65b6c9c3dd
Jonathan Cunanan <j_cunanan05@yahoo.com>
2018-07-11 17:28:41 -0400

    Add Fade effect on Quote Load

--- src/components/BlockQuote.js
+++ src/components/BlockQuote.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+
+const QuoteHeader = props => (
+  <h2 id="text" className="title title__text has-text-centered">
+    <span className="double-quote">"</span>
+      {props.text}
+    <span className="double-quote">"</span>
+  </h2>
+);
+
+const QuoteFooter = props => (
+  <footer>
+    <p id="author" className="subtitle subtitle__author">
+      - <em>{props.author}</em>
+    </p>
+  </footer>
+);
+
+function fadeTranstion(jsx, key) {
+  return (
+    <CSSTransition classNames="fade"
+      key={key}
+      timeout={{ enter: 500, exit: 300 }} >
+      { jsx }
+    </CSSTransition>
+  );
+}
 
 const BlockQuote = props => (
-  <blockquote className="blockquote">
-    <h2 id="text" className="title title__text has-text-centered">
-      <span className="double-quote">"</span>
-      {props.quote.text}
-      <span className="double-quote">"</span>
-    </h2>
+  <TransitionGroup 
+    component="blockquote"
+    className="blockquote" >
 
-    <footer>
-      <p id="author" className="subtitle subtitle__author">
-        - <em>{props.quote.author}</em>
-      </p>
-    </footer>
-  </blockquote>
+    {fadeTranstion(<QuoteHeader text={props.quote.text} />, props.quote.text)}
+
+    {fadeTranstion(<QuoteFooter author={props.quote.author} />, props.quote.author)}
+  </TransitionGroup>
 );
 
 export default BlockQuote;
